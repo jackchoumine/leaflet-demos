8974d6baa0d7519c4819e2e106799c44f1b57d15
jack <jackzhoumine@gmail.com>
2024-04-18 21:09:53 +0800

    chore: 引入antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Menu } from 'antd'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [selectedKey, setSelectedKey] = useState('home')
+
+  const handleMenuClick = e => {
+    setSelectedKey(e.key)
+    // 根据选中的菜单项 key 值渲染对应的内容
+    renderContent(e.key)
+  }
+
+  const renderContent = key => {
+    switch (key) {
+      case 'home':
+        return <div>Home Content</div>
+      case 'about':
+        return <div>About Content</div>
+      case 'contact':
+        return <div>Contact Content</div>
+      default:
+        return null
+    }
+  }
+
+  const menuItems = [
+    { label: 'Home', key: 'home' },
+    { label: 'About', key: 'about' },
+    { label: 'Contact', key: 'contact' },
+  ]
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <div>
+      <Menu
+        mode='horizontal'
+        selectedKeys={[selectedKey]}
+        onClick={handleMenuClick}
+        items={menuItems}
+      />
+      <div>{renderContent(selectedKey)}</div>
+    </div>
   )
 }
 
