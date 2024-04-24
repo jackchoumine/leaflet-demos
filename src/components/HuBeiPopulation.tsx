b3ea36f594264688f0eeac38c5b12ab7b2f32a58
ZhouQiJun <zhouqijun4job1@163.com>
2024-04-24 09:21:21 +0800

    feat: 鼠标移入图层，高亮显示区域，移除恢复

--- src/components/HuBeiPopulation.tsx
+++ src/components/HuBeiPopulation.tsx
@@ -1,15 +1,26 @@
+/*
+ * @Author      : ZhouQiJun
+ * @Date        : 2024-04-23 19:05:26
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2024-04-24 09:19:45
+ * @Description : 分级统计图法 -- 展示湖北省人口分布
+ */
 import { useEffect, useRef } from 'react'
 import { useGaoDeMap } from '../hooks'
 import L from 'leaflet'
 import { style } from '../data/huBei'
 import { Legend } from '../plugins'
+
 // import huBeiGeoJson from '../data/hubei.json'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 // const huBeiGeoJson = require('../data/Hubei.json')
+
 export function HuBeiPopulation() {
   const { WithAMap, mapInstance } = useGaoDeMap()
+
   const jsonLayer = useRef(null)
   const _legend = useRef(null)
+
   useEffect(() => {
     const wuHanCenter = [30.584355, 114.298572]
     mapInstance.current?.setView(wuHanCenter, 14)
@@ -17,24 +28,26 @@ export function HuBeiPopulation() {
       const layer = new L.GeoJSON(huBeiGeoJson, {
         style,
         onEachFeature: (feature, layer) => {
-          // 如何才能让注记的大小随着比例尺的变化而变化呢?
-          // NOTE
-          const latlng = layer.getBounds().getCenter()
-          L.marker(latlng, {
+          // TODO 如何才能让注记的大小随着比例尺的变化而变化呢?
+          const latLng = layer.getBounds().getCenter()
+          L.marker(latLng, {
             icon: L.divIcon({
               className: 'polygon-label',
               html: feature.properties.name,
               iconSize: [100, 20],
-              // 禁止交互
-              interactive: false,
+              // 禁止交互 这里不起作用
+              // interactive: false,
             }),
+            // 禁止交互
+            interactive: false,
           }).addTo(mapInstance.current)
+
+          layer.on({
+            mouseover: hightFeature,
+            mouseout: resetHighlight,
+            click: zoomToFeature,
+          })
         },
-      })
-      layer.on({
-        mouseover: hightFeature,
-        mouseout: resetHighlight,
-        // click: zoomToFeature,
       })
       layer.addTo(mapInstance.current)
       mapInstance.current?.fitBounds(layer.getBounds())
@@ -46,29 +59,29 @@ export function HuBeiPopulation() {
     return () => {
       _legend.current?.remove()
     }
+    // 使用到了mapInstance.current 需要加入依赖或者定义到 useEffect 内部
+    function zoomToFeature(e) {
+      mapInstance.current.fitBounds(e.target.getBounds())
+    }
   }, [mapInstance])
 
   return <WithAMap />
 
   function hightFeature(e) {
-    console.log(e.target)
+    // console.log(e.target)
     const layer = e.target
     layer.setStyle({
-      weight: 5,
-      color: '#666',
+      weight: 3,
+      color: 'blue',
       dashArray: '',
       fillOpacity: 0.7,
     })
     // if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-    // layer.bringToFront()
+    layer.bringToFront()
     // }
   }
 
   function resetHighlight(e) {
     jsonLayer.current.resetStyle(e.target)
-  }
-
-  function zoomToFeature(e) {
-    mapInstance.current.fitBounds(e.target.getBounds())
   }
 }
