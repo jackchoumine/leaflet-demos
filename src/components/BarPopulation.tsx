3e474b3c700543589f300ed482568d860d687a84
ZhouQiJun <zhouqijun4job1@163.com>
2024-04-25 20:00:41 +0800

    feat: 整合数据

--- src/components/BarPopulation.tsx
+++ src/components/BarPopulation.tsx
@@ -2,24 +2,51 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-04-25 18:03:09
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2024-04-25 18:34:34
+ * @LastEditTime: 2024-04-25 19:56:35
  * @Description :
  */
 import { useEffect } from 'react'
 import { useGaoDeMap } from '../hooks'
+import L from 'leaflet'
+import * as d3 from 'd3'
 
 export function BarPopulation() {
   const { WithAMap, mapInstance } = useGaoDeMap()
+
+  const arr = []
+
   useEffect(() => {
     const wuHanCenter = [30.584355, 114.298572]
     mapInstance.current?.setView(wuHanCenter, 6)
     import('../data/华中地区2021.json').then(geoJSON => {
-      L.geoJSON(geoJSON, {
+      const huaZhongLayer = L.geoJSON(geoJSON, {
         style,
-      }).addTo(mapInstance.current)
+        onEachFeature: (feature, layer) => {
+          if (arr.length < 3) {
+            arr.push({
+              name: feature.properties.name,
+              // 获取各省份的中心点
+              latLng: layer.getBounds().getCenter(),
+            })
+          }
+        },
+      })
+      huaZhongLayer.addTo(mapInstance.current)
+      // FIXME 监听不到图层加载事件
+      huaZhongLayer.on('add',e=>{
+        console.log('add',e)
+      })
+      d3.csv('/华中人口数.csv').then(csvTable => {
+        arr.forEach(item => {
+          const row = csvTable.find(row => row.name === item['省份'])
+          item.female = +row['女']
+          item.male = +row['男']
+        })
+        console.log(arr)
+      })
     })
-    // L.geoJSON(huBeiGeoJson, {}).addTo(mapInstance.current)
   }, [mapInstance])
+
   return <WithAMap />
 
   function style(feature) {
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,9 +2,10 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-04-23 18:31:26
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2024-04-25 10:50:41
+ * @LastEditTime: 2024-04-25 18:24:51
  * @Description : 
  */
 export * from './ZoomViewer'
 export * from './Legend'
 export * from './PopulationInfo'
+export * from './D3SvgOverlay'
