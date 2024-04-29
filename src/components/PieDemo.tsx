b9bfc191a2777e7eff8503ea133dfe9057a7c05c
jack <jackzhoumine@gmail.com>
2024-04-29 23:46:26 +0800

    feat: 绘制饼图

--- src/components/PieDemo.tsx
+++ src/components/PieDemo.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useTianDiTuMap } from '../hooks'
 import { GeoJSON } from 'leaflet'
 import * as d3 from 'd3'
+import { D3SvgOverlay } from '../plugins'
 
 export function PieDemo() {
   const { WithTianDiTuMap, mapInstance } = useTianDiTuMap()
@@ -29,6 +30,56 @@ export function PieDemo() {
           // item.tertiary = +json[item.name]['第三产业增加值']
           item.third = +json[item.name]['第三产业增加值']
         })
+        console.log(arr)
+        var pie = d3.pie()
+        //返回一个由10种不同颜色组成的数组
+        var color = d3.scaleOrdinal(d3.schemeCategory10)
+
+        const pieOverLayer = new D3SvgOverlay((sel, proj) => {
+          var selArcs = sel.selectAll('g.arc')
+          arr.forEach(item => {
+            let arrBinding = []
+            const { first, second, third } = item
+            arrBinding.push(first)
+            arrBinding.push(second)
+            arrBinding.push(third)
+            const sum = first + second + third
+            const outerRadius = Math.sqrt(sum / 50)
+            const innerRadius = 20
+            const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius)
+            // 绘制扇形
+            const arcs = selArcs
+              .data(pie(arrBinding))
+              .enter()
+              .append('g')
+              .attr('class', 'arc')
+              .attr('transform', function (d) {
+                // console.log(item)
+                // 饼状图圆心坐标
+                const { x, y } = proj.latLngToLayerPoint(item.latLng)
+                const centerOfCircle = `translate(${x},${y})`
+                return centerOfCircle
+              })
+
+            //通过路径绘制弧
+            arcs
+              .append('path')
+              .attr('fill', function (d, i) {
+                return color(i) //设置填充颜色
+              })
+              .attr('d', arc)
+              // NOTE pointer-events 与 leaflet.css 里面样式设置冲突，导致鼠标提示失效，故增加以下语句
+              .attr('style', 'pointer-events: visiblepainted')
+            arcs
+              .append('title') //鼠标提示
+              .text(function (d, i) {
+                const title = `第${i + 1}产业增加值：${d.value}${json['单位']}`
+                return title
+              })
+            arrBinding = []
+          })
+        })
+        pieOverLayer.addTo(mapInstance.current)
       })
     })
   }, [mapInstance])
